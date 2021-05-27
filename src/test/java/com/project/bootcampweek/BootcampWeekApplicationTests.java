352311970e3351ed65e0122df68434f44c217270
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-05-27 01:10:15 -0300

    First commit

---
+++ src/test/java/com/project/bootcampweek/BootcampWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bootcampweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
