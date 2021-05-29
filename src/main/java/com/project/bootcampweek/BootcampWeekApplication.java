095360d8870047fe190b3aa2be17f409c1866d60
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-05-29 01:35:40 -0300

    Test heroku deploy

--- src/main/java/com/project/bootcampweek/BootcampWeekApplication.java
+++ src/main/java/com/project/bootcampweek/BootcampWeekApplication.java
@@ -20,7 +20,7 @@ public class BootcampWeekApplication {
 	public OpenAPI customOpenAPI(@Value("${application.description}") String description){ //http://localhost:3600/bootcamp/swagger-ui/index.html?
 		return new OpenAPI().info(new Info()
 				.title(description)
-				.version("1.0")
+				.version("2.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
